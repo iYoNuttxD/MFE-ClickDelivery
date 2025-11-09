c9a8425144907e7e51da51bd1bb00582a67daa3b
JoaoVitorSHernandes <joao.hernandes@pucpr.edu.br>
2025-11-09 16:10:34 -0300

    Disable eslint rule for I18nProvider component

--- src/app/providers/I18nProvider.tsx
+++ src/app/providers/I18nProvider.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-refresh/only-export-components */
 import React from 'react';
 import i18n from 'i18next';
 import { initReactI18next, I18nextProvider } from 'react-i18next';
